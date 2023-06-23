bf8b3fb607a422c01b5b17553c837b67beab38a0
SirKunle <99807539+SirKunle@users.noreply.github.com>
2023-06-23 15:34:35 +0100

    Add files via upload

---
+++ training2.js
@@ -0,0 +1,10 @@
+function validate(){
+    var username = document.getElementById("username").Value;
+    var password = document.getElementById("password").Value;
+    if (username==="admin" && password === "user"){
+        alert("login successfully");
+      
+    }else{
+        alert("login failed")
+    }
+}
